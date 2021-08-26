218f5d9147fba7b84af4bcdb753939d55de18bb6
lazywalker <lazywalkerz@gmail.com>
2021-08-27 01:43:10 +0800

    remove reqwest

--- src/device.rs
+++ src/device.rs
@@ -1,9 +1,10 @@
+use hyper::{Body, Method, Request};
 use ini::Ini;
 use lazy_static::lazy_static;
 use log::{debug, info};
 use md5;
 use postgres::{Client, NoTls};
-use std::{collections::HashMap};
+use std::collections::HashMap;
 use tokio::sync::Mutex;
 
 use crate::util;
@@ -98,28 +99,39 @@ pub async fn get_port(sn: &str) -> Option<u16> {
     let secret = sec.get("api_secret").unwrap();
     let sign_orig = format!("timestamp={}&secret={}", timestamp, secret);
     let sign = format!("{:x}", md5::compute(sign_orig));
-    let params = [("sn", sn)];
 
-    let client = reqwest::Client::new();
-    let res = client
-        .post(&api_url)
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(&api_url)
+        .header("content-type", "application/x-www-form-urlencoded")
+        .header("accept", "*/*")
         .header("timestamp", timestamp)
         .header("sign", &sign)
-        .form(&params)
-        .send()
-        .await
+        .body(Body::from(format!("sn={}", sn)))
         .unwrap();
-    // println!("{:}", &sign);
-    // println!("{:#?}", res.json::<serde_json::Value>().await.unwrap());
-    match res.json::<serde_json::Value>().await {
-        Ok(it) => match it["data"]["port"].to_string().parse::<u16>() {
-            Ok(port) => match port {
-                0 => None,
-                _ => Some(port),
-            },
-            _ => None,
-        },
-        _ => None,
+    let client = hyper::Client::new();
+
+    // POST it...
+    match client.request(req).await {
+        Ok(resp) => {
+            let body_vec = hyper::body::to_bytes(resp.into_body())
+                .await
+                .unwrap()
+                .to_vec();
+            let body = String::from_utf8_lossy(&body_vec);
+            println!("Body: {}", body);
+            match serde_json::from_str::<serde_json::Value>(&body) {
+                Ok(v) => match v["data"]["port"].to_string().parse::<u16>() {
+                    Ok(port) => match port {
+                        0 => None,
+                        _ => Some(port),
+                    },
+                    _ => None,
+                },
+                _ => None,
+            }
+        }
+        Err(_) => None,
     }
 }
 
